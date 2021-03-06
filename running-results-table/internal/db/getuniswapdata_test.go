9cf4ad43711273c18f758f84e2df713d7a91d6f8
Yaroslav Afonin <ya1220@ic.ac.uk>
2021-03-06 18:48:51 +0300

    Uniswap test - YA

--- running-results-table/internal/db/getuniswapdata_test.go
+++ running-results-table/internal/db/getuniswapdata_test.go
@@ -6,9 +6,8 @@ import (
 	"github.com/machinebox/graphql"
 )
 
-func TestUniswapDataOutput(t *testing.T) {
+func TestUniswapDataDownload(t *testing.T) {
 	database := New()
-
 	clientUniswap := graphql.NewClient("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
 
 	reqUniswapHist := graphql.NewRequest(`
@@ -45,14 +44,53 @@ func TestUniswapDataOutput(t *testing.T) {
 	U := UniswapInputStruct{clientUniswap, reqUniswapIDFromTokenTicker, reqUniswapHist}
 	getUniswapData(&database, U)
 
+	// fmt.Println(len(database.historicalcurrencydata))
+	// fmt.Println(len(database.historicalcurrencydata[0].Ticker))
+
 	// Test 1
 	if len(database.historicalcurrencydata) == 0 {
 		t.Errorf("Unsufficient data downloaded!")
 	}
 
 	// Test 2
-	if len(database.historicalcurrencydata[0].Ticker) == null {
-		t.Errorf("Ticker ")
+
+	// Test 3
+	var teststringarray []string
+
+	for i := 0; i < len(database.historicalcurrencydata); i++ {
+		teststringarray = append(teststringarray, database.historicalcurrencydata[i].Ticker)
+		if len(database.historicalcurrencydata[i].Ticker) == 0 {
+			t.Errorf("No data pulled")
+		}
+		if len(database.historicalcurrencydata[i].Date) == 0 {
+			t.Errorf("No data pulled")
+		}
+		if len(database.historicalcurrencydata[i].Price) == 0 {
+			t.Errorf("No data pulled")
+		}
+		if len(database.historicalcurrencydata[i].Date) > 100 {
+			t.Errorf("Too much data pulled")
+		}
+		if len(database.historicalcurrencydata[i].Price) > 100 {
+			t.Errorf("Too much data pulled")
+		}
+
+	}
+
+	if !stringInSlice("ETH", teststringarray) && !stringInSlice("WETH", teststringarray) {
+		t.Errorf("ETH data missing!")
+	}
+	if !stringInSlice("DAI", teststringarray) {
+		t.Errorf("DAI data missing!")
+	}
+	if !stringInSlice("USDC", teststringarray) {
+		t.Errorf("USDC data missing!")
+	}
+	if !stringInSlice("USDT", teststringarray) {
+		t.Errorf("USDT data missing!")
+	}
+	if !stringInSlice("WBTC", teststringarray) {
+		t.Errorf("WBTC data missing!")
 	}
 
 }
