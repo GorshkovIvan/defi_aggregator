45b5a48264d2b85573e8ddd085e00ae8f8be02f2
John Kwon <JohnKwon@Johns-MacBook-Pro.local>
2021-03-07 00:39:39 +0900

    balancer tests

---
+++ running-results-table/internal/db/getbalancerdata_test.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/machinebox/graphql"
+)
+
+func TestgetBalancerData(t *testing.T) {
+	database := New()
+	clientUniswap := graphql.NewClient("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
+	reqUniswapHist := graphql.NewRequest(`
+				query ($tokenid:String!){
+						tokenDayDatas(first: 30 orderBy: date, orderDirection: asc,
+						 where: {
+						   token:$tokenid
+						 }
+						) {
+						   date
+						   priceUSD
+						   token{
+							   id
+							   symbol
+						   }
+						}
+				  }
+			`)
+
+	reqUniswapIDFromTokenTicker := graphql.NewRequest(`
+						query ($ticker:String!){
+							tokens(where:{symbol:$ticker})
+							{
+								id
+								symbol
+							}
+						}
+			`)
+
+	reqUniswapIDFromTokenTicker.Header.Set("Cache-Control", "no-cache")
+	reqUniswapHist.Header.Set("Cache-Control", "no-cache")
+
+	U := UniswapInputStruct{clientUniswap, reqUniswapIDFromTokenTicker, reqUniswapHist}
+
+	getBalancerData(&database, U)
+
+	if len(database.historicalcurrencydata) != 30 {
+		t.Errorf("fail!")
+	}
+}
+
+
+
+	// Test 1
+	
+	
