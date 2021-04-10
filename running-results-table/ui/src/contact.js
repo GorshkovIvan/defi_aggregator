7a5985d3a6036679aa1ced14761da4cee0cd6a9e
John Kwon <JohnKwon@Johns-MacBook-Pro.local>
2021-04-10 17:45:40 +0900

    about and contact pages

--- running-results-table/ui/src/contact.js
+++ running-results-table/ui/src/contact.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { keepTheme } from './themes.js';
 
 function Contact() {
-    
+
+    useEffect(() => { keepTheme(); })
+
     return (
-        <div>
-            <p>Contact</p>
+        <div class="Contact">
+            <h1>High Yield 4 Me</h1>
+            <h4>Contact Us</h4>
+            <div class="back">
+                <Link to="/">
+                    <div class="ui animated fade button" tabindex="0">
+                        <div class="visible content">Back</div>
+                        <div class="hidden content">
+                            <i class="left arrow icon" />
+                        </div>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
